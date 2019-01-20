087f4a98ea7a5c6a8a6dab7738b748f27665db31
Tomas Basham <tomasbasham@gmail.com>
2019-01-20 14:29:53 +0000

    Update test suite

--- tests/unit/services/pubnub-test.js
+++ tests/unit/services/pubnub-test.js
@@ -1,11 +1,11 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('service:pubnub', 'Unit | Service | pubnub', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+module('Unit | Service | pubnub', function(hooks) {
+  setupTest(hooks);
 
-test('it creates a PubNub object', function(assert) {
-  let service = this.subject();
-  assert.ok(service);
+  test('it creates a PubNub object', function(assert) {
+    let service = this.owner.lookup('service:pubnub');
+    assert.ok(service);
+  });
 });
--- tests/unit/utils/event-string-test.js
+++ tests/unit/utils/event-string-test.js
@@ -1,14 +1,17 @@
-import { messageEventString, presenceEventString } from 'ember-cli-pubnub/utils/event-string';
+import {
+  messageEventString,
+  presenceEventString
+} from 'ember-cli-pubnub/utils/event-string';
 import { module, test } from 'qunit';
 
-module('Unit | Utility | event string');
+module('Unit | Utility | event string', function() {
+  test('it returns a message event string scoped to a channel', function(assert) {
+    let result = messageEventString('enterprise');
+    assert.equal('pn-message:enterprise', result);
+  });
 
-test('it returns a message event string scoped to a channel', function(assert) {
-  let result = messageEventString('enterprise');
-  assert.equal('pn-message:enterprise', result);
-});
-
-test('it returns a presence event string scoped to a channel', function(assert) {
-  let result = presenceEventString('enterprise');
-  assert.equal('pn-presence:enterprise', result);
+  test('it returns a presence event string scoped to a channel', function(assert) {
+    let result = presenceEventString('enterprise');
+    assert.equal('pn-presence:enterprise', result);
+  });
 });
